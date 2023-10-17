a97c48afd15a9d442cf0abc7994643264a955fff
BRIAN MAHER <bmaher22@mac.comclear>
2023-10-17 18:02:48 -0400

    pushing to the hub, then heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
-
-const PORT = 3001;
+const data = require('./db/db.json')
+const PORT = process.env.PORT || 3001;
+// Helper functions for reading and writing to the JSON file
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const app = express();
 
@@ -14,15 +16,28 @@ app.use(express.static('public'));
 
 // this will be retreiving the files 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body)
+
+    readAndAppend(req.body, './db/db.json');
+    res.json(`Notw added`);
+});
+
+
 // this will be returning the notes being requested
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
